9911153d50db6ec755c7f1f2b92904f9d1bd6ce3
Maxime Culea <culea.maxime@gmail.com>
2017-12-01 17:09:53 +0100

    add helpers to parse content, to be finished

---
+++ classes/helper/helper.php
@@ -0,0 +1,7 @@
+<?php namespace BEA\Find_Media\Helper;
+
+class Helper {
+	static function get_media_from_text( $text ) {
+		return [];
+	}
+}
---
+++ classes/helper/post.php
@@ -0,0 +1,15 @@
+<?php namespace BEA\Find_Media\Helper;
+
+class Post extends Helper {
+	static function get_media_from_post_acf_fields( $post_id ) {
+		return [];
+	}
+
+	static function get_media_from_post_meta( $post_id ) {
+		return [];
+	}
+
+	static function get_media_from_post_thumbnail( $post_id ) {
+		return [];
+	}
+}
