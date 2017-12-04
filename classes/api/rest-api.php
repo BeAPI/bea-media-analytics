083269363e76f98869dc4a71dfe90b70d160e179
Maxime Culea <culea.maxime@gmail.com>
2017-12-05 00:05:02 +0100

    add rest api field

--- classes/api/rest-api.php
+++ classes/api/rest-api.php
@@ -1,11 +1,48 @@
 <?php namespace BEA\Find_Media\API;
 
+use BEA\Find_Media\DB;
 use BEA\Find_Media\Singleton;
 
 class Rest_Api {
 	use Singleton;
 
 	protected function init() {
-		// TODO : add data to rest api
+		add_filter( 'rest_api_init', [ $this, 'rest_api_add_custom_fields' ] );
+	}
+
+	/**
+	 * Register custom fields against attachment post type
+	 *
+	 * @author Maxime CULEA
+	 */
+	public function rest_api_add_custom_fields() {
+		if ( ! function_exists( 'register_rest_field' ) ) {
+			return;
+		}
+
+		register_rest_field( [ 'attachment' ], 'bea_find_media_counter', [
+			'get_callback' => [ $this, 'get_media_counter' ],
+			'schema'       => [
+				'description' => 'Display the attachment\'s counter for media use.',
+				'type'        => 'int',
+				'context'     => [ 'view', 'edit' ],
+			],
+		] );
+	}
+
+	/**
+	 * Get attachment's counter (use)
+	 *
+	 * @param $post
+	 * @param $field_name
+	 * @param $request
+	 *
+	 * @author Maxime CULEA
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_media_counter( $post, $field_name, $request ) {
+		return (string) DB::get_counter( $post['id'] );
 	}
 }
