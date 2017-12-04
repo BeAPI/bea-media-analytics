ffdc6efab35192dfbe9fbfcafa9dba3360931ca3
Maxime Culea <culea.maxime@gmail.com>
2017-12-05 00:10:55 +0100

    refactor textdomain

--- compat.php
+++ compat.php
@@ -17,12 +17,12 @@ class Compatibility {
 		}
 
 		// Load the textdomain
-		load_plugin_textdomain( 'bea-cpt-hub', false, BEA_FIND_MEDIA_PLUGIN_DIRNAME . '/languages' );
+		load_plugin_textdomain( 'bea-find-media', false, BEA_FIND_MEDIA_PLUGIN_DIRNAME . '/languages' );
 
-		trigger_error( sprintf( __( 'Plugin Boilerplate requires PHP version %s or greater to be activated.', 'bea-cpt-hub' ), BEA_FIND_MEDIA_MIN_PHP_VERSION ) );
+		trigger_error( sprintf( __( 'Plugin Boilerplate requires PHP version %s or greater to be activated.', 'bea-find-media' ), BEA_FIND_MEDIA_MIN_PHP_VERSION ) );
 
 		// Deactive self
-		deactivate_plugins( BEA_FIND_MEDIA_DIR . 'bea-cpt-hub.php' );
+		deactivate_plugins( BEA_FIND_MEDIA_DIR . 'bea-find-media.php' );
 
 		unset( $_GET['activate'] );
 
@@ -34,7 +34,7 @@ class Compatibility {
 	 */
 	public static function admin_notices() {
 		echo '<div class="notice error is-dismissible">';
-		echo '<p>' . esc_html( sprintf( __( 'Plugin Boilerplate require PHP version %s or greater to be activated. Your server is currently running PHP version %s.', 'bea-cpt-hub' ), BEA_FIND_MEDIA_MIN_PHP_VERSION, PHP_VERSION ) ) . '</p>';
+		echo '<p>' . esc_html( sprintf( __( 'Plugin Boilerplate require PHP version %s or greater to be activated. Your server is currently running PHP version %s.', 'bea-find-media' ), BEA_FIND_MEDIA_MIN_PHP_VERSION, PHP_VERSION ) ) . '</p>';
 		echo '</div>';
 	}
 }
