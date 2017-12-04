1403f0a07e5d8b57ba236eaed9f3202b79187503
Maxime Culea <culea.maxime@gmail.com>
2017-12-04 15:28:07 +0100

    format data for use into admin table

--- classes/main.php
+++ classes/main.php
@@ -3,5 +3,25 @@
 class Main {
 	use Singleton;
 
-	protected function init() {}
+	protected function init() {
+		add_filter( 'bea.find_media.db.get_data', [ $this, 'format_indexed_values' ], 100 );
+	}
+
+	/**
+	 * @param $data
+	 */
+	public function format_indexed_values( $data ) {
+		if ( empty( $data ) ) {
+			return [];
+		}
+
+		$out = [];
+		foreach ( $data as $_d ) {
+			if ( ! isset( $out[$_d->object_type][$_d->object_id][$_d->media_id] ) || ! in_array( $_d->type, $out[$_d->object_type][$_d->object_id][$_d->media_id] ) ) {
+				$out[$_d->object_type][$_d->object_id][$_d->media_id] = [$_d->type];
+			}
+		}
+
+		return $out;
+	}
 }
