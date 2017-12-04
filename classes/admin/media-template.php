fd726e80ab7ce2d77d092bd36e35d11d03b878f2
Maxime Culea <culea.maxime@gmail.com>
2017-12-04 15:28:29 +0100

    add the fields for the media modal and edit

--- classes/admin/media-template.php
+++ classes/admin/media-template.php
@@ -7,30 +7,55 @@ class Media_Template {
 	use Singleton;
 
 	protected function init() {
-		add_action( 'wp_enqueue_media', [ $this, 'javascript_for_view' ], 99 );
+		add_filter( 'attachment_fields_to_edit', [ $this, 'modal_view' ], 20, 2 );
+		add_filter( 'attachment_fields_to_edit', [ $this, 'edit_view' ], 20, 2 );
 	}
 
-	public function javascript_for_view() { ?>
-		<script type='application/javascript'>
-            var params = {};
-            window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (str, key, value) {
-                params[key] = value;
-            });
-            if ( params && 0 != params.item.length) {
-                jQuery('.attachment-info .settings').append('<p>' + params.item + '</p>');
-            }
-		</script>
-		<?php
+	/**
+	 * Display into media's modal, the number of indexed
+	 *
+	 * @param $form_fields
+	 * @param $media
+	 *
+	 * @since 1.0.0
+	 * @author Maxime CULEA
+	 *
+	 * @return array
+	 */
+	public function modal_view( $form_fields, $media ) {
+		$counter = DB::get_counter( $media->ID );
+		if ( ! empty( $counter ) ) {
+			$html = sprintf( '<a href="%s" type="text" >%s</a>', get_edit_post_link( $media->ID ), esc_html( $counter ) );
+		} else {
+			$html = '<span>0</span>';
+		}
+		$form_fields['bea_find_media'] = array(
+			'label'         => __( 'Time used', 'bea-find-media' ),
+			'input'         => 'html',
+			'html'          => $html,
+			'show_in_edit'  => false,
+			'show_in_modal' => true,
+		);
 
-		/**
-		 * '<label class="setting">' +
-		 * '<span class="name">Times used</span>' +
-		 * '<span class="value">' +
-		 * '<a href="post.php?post=' + params.item + '&action=edit">' +
-		 * wp.media.attachment(params.item).attributes.bea_find_media_counter; +
-		 * '</a>' +
-		 * '</span>' +
-		 * '</label>'
-		 */
+		return $form_fields;
+	}
+
+	public function edit_view( $form_fields, $media ) {
+		$data = DB::get_data( $media->ID );
+		$amlt = new Admin_Media_List_Table();
+
+		ob_start();
+		$amlt->display();
+		$html = ob_get_clean();
+
+		$form_fields['bea_find_media'] = array(
+			'label'         => __( 'Time used', 'bea-find-media' ),
+			'input'         => 'html',
+			'html'          => $html,
+			'show_in_edit'  => true,
+			'show_in_modal' => false,
+		);
+
+		return $form_fields;
 	}
 }
