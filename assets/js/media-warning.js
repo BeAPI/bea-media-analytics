170595b56dbec6e3687f2225358ef9a70bcf3f9e
Maxime Culea <culea.maxime@gmail.com>
2017-12-07 14:37:58 +0100

    #17 : handle the warning popup

--- assets/js/media-warning.js
+++ assets/js/media-warning.js
@@ -1,7 +1,8 @@
 (function ($, w) {
     w.bea_find_media_warn = function (media_id) {
         var string = bea_find_media.i18n.warning_confirm;
-        var counter = jQuery('#post-'+media_id).find('.bea-find-media-counter a').text();
-        return confirm(string.replace('%s', counter));
+        var counter = jQuery('#post-' + media_id).find('.bea-find-media-counter a').text();
+        var counter_label = Number(counter) > 1 ? bea_find_media.i18n.time_plural : bea_find_media.i18n.time_singular;
+        return confirm(string.replace('%s', counter + ' ' + counter_label));
     }
 })(jQuery, window);
