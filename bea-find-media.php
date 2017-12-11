56a3860642abdcbda3f6f9b069ca76403818537b
Maxime Culea <culea.maxime@gmail.com>
2017-12-12 00:49:44 +0100

    Bump version

--- bea-find-media.php
+++ bea-find-media.php
@@ -1,7 +1,7 @@
 <?php
 /*
  Plugin Name: BEA - Find Media
- Version: 1.0.0
+ Version: 1.0.1
  Plugin URI: https://github.com/BeAPI/bea-find-media
  Description: Find where medias are used across your site.
  Author: Be API Technical team
@@ -34,7 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Plugin constants
-define( 'BEA_FIND_MEDIA_VERSION', '1.0.0' );
+define( 'BEA_FIND_MEDIA_VERSION', '1.0.1' );
 define( 'BEA_FIND_MEDIA_MIN_PHP_VERSION', '7.0' );
 
 // Plugin URL and PATH
@@ -62,15 +62,15 @@ register_activation_hook( __FILE__, [ '\BEA\Find_Media\Plugin', 'activate' ] );
 register_deactivation_hook( __FILE__, [ '\BEA\Find_Media\Plugin', 'deactivate' ] );
 
 add_action( 'plugins_loaded', 'plugins_loaded_bea_find_media_plugin' );
-/**
- * Init the plugin
- */
+/** Init the plugin */
 function plugins_loaded_bea_find_media_plugin() {
 	// DB
 	\BEA\Find_Media\DB_Table::get_instance();
 
 	// Client
 	\BEA\Find_Media\Main::get_instance();
+	// Addons
+	\BEA\Find_Media\Addons\Main::get_instance();
 
 	// Api
 	\BEA\Find_Media\API\Json::get_instance();
@@ -79,7 +79,7 @@ function plugins_loaded_bea_find_media_plugin() {
 	// WP Cli
 	\BEA\Find_Media\WP_Cli\Main::get_instance();
 
-	// Admin
+	// Admin or wp-cli context
 	if ( is_admin() || defined( 'WP_CLI' ) ) {
 		\BEA\Find_Media\Admin\Main::get_instance();
 		\BEA\Find_Media\Admin\Post::get_instance();
