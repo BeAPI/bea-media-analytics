210294e3675304218b70094501dcf10bce1f4298
Maxime Culea <culea.maxime@gmail.com>
2017-12-11 01:39:28 +0100

    #35 : hook on get data for CSF and change way to reorder data because of MS

--- classes/addons/content-sync-fusion/main.php
+++ classes/addons/content-sync-fusion/main.php
@@ -18,6 +18,10 @@ class Main {
 		add_filter( 'bea.find_media.media.edit_view_title', [ $this, 'edit_view_title' ], 20, 2 );
 
 		add_filter( 'bea.find_media.db.get_counter', [ $this, 'get_counter' ], 20, 2 );
+		add_filter( 'bea.find_media.db.get_data', [ $this, 'get_data' ], 20, 2 );
+		// Filter array values for MS
+		remove_filter( 'bea.find_media.db.get_data', [ 'BEA\Find_Media\Main', 'format_indexed_values' ], 100 );
+		add_filter( 'bea.find_media.db.get_data', [ 'BEA\Find_Media\Main', 'format_indexed_values_ms' ], 120 );
 	}
 
 	/**
@@ -137,5 +141,57 @@ class Main {
 		}
 
 		return $title;
+	}
+
+	/**
+	 * Get all medias data from emitter and receivers sites :
+	 * - the emitter usages is the data value
+	 * - the receivers usages is what is added on
+	 * Final array is gathered by blog_id.
+	 *
+	 * @param $data
+	 * @param $media_id
+	 *
+	 * @since 1.0.1
+	 * @author Maxime CULEA
+	 *
+	 * @return array
+	 */
+	public function get_data( $data, $media_id ) {
+		$db_table = DB_Table::get_instance();
+		if ( ! $db_table->table_exists() ) {
+			return $data;
+		}
+
+		$emitter_blog_id = get_current_blog_id();
+
+		// Get attachment syncs from current blog_id
+		$syncs = Helper::get_syncronizations( $emitter_blog_id );
+		if ( empty( $syncs ) ) {
+			return $data;
+		}
+
+		// Get receivers blogs ids from synchronizations
+		$receivers_blogs_ids = Helper::get_receivers_blogs_ids( $syncs );
+		if ( empty( $receivers_blogs_ids ) ) {
+			// No receivers
+			return $data;
+		}
+
+		$data_by_blog                     = [];
+		$data_by_blog[ $emitter_blog_id ] = $data;
+		$table_name                       = DB_Table::get_instance()->get_table_name();
+		foreach ( $receivers_blogs_ids as $receiver_blog_id ) {
+			// Get the receiver media id
+			$receiver_media_id = Helper::get_receiver_obj_id_from_emitter_obj_id( $emitter_blog_id, $receiver_blog_id, $media_id );
+			// Get receiver media id usages
+			$_data = $db_table->db->get_results( $db_table->db->prepare( "SELECT * FROM " . $table_name . " WHERE blog_id = %d AND media_id = %d", $receiver_blog_id, $receiver_media_id ) );
+			if ( empty( $_data ) ) {
+				continue;
+			}
+			$data_by_blog[ $receiver_blog_id ] = $_data;
+		}
+
+		return $data_by_blog;
 	}
 }
