c24a27b9dd7ff7dd28cc3acf4429f5bd31cda78d
Maxime Culea <culea.maxime@gmail.com>
2017-12-12 15:37:51 +0100

    #5 : use cron class from activation / deactivation

--- classes/plugin.php
+++ classes/plugin.php
@@ -3,17 +3,23 @@
 class Plugin {
 	use Singleton;
 
+	/**
+	 * Plugin activation
+	 */
 	public static function activate() {
 		DB_Table::get_instance()->upgrade_database();
 
 		// For safety, delete existing data
 		DB::get_instance()->delete_blog( get_current_blog_id() );
 
-		// Index all content with a cron
-		wp_schedule_single_event( time() + 60, 'bea.find_media.cron.force_indexation' );
+		Cron::schedule();
 	}
 
+	/**
+	 * Plugin deactivation
+	 */
 	public static function deactivate() {
 		DB::get_instance()->delete_blog( get_current_blog_id() );
+		Cron::unschedule();
 	}
 }
