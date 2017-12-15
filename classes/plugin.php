7d6e36023fe3c0c22e093677f73bd874cb267cdd
Maxime CULEA <MaximeCulea@users.noreply.github.com>
2017-12-15 16:36:00 +0100

    Fix stupid class renaming

--- classes/plugin.php
+++ classes/plugin.php
@@ -12,7 +12,7 @@ class Plugin {
 		// For safety, delete existing data
 		DB::get_instance()->delete_blog( get_current_blog_id() );
 
-		Cron::schedule();
+		Crons::schedule();
 	}
 
 	/**
@@ -20,6 +20,6 @@ class Plugin {
 	 */
 	public static function deactivate() {
 		DB::get_instance()->delete_blog( get_current_blog_id() );
-		Cron::unschedule();
+		Crons::unschedule();
 	}
 }
