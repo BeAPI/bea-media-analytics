6aedab62a8dd5d9a9cd8c597383489af6378a786
Maxime CULEA <MaximeCulea@users.noreply.github.com>
2017-12-22 12:50:36 +0100

    Update bea-find-media.php

--- bea-find-media.php
+++ bea-find-media.php
@@ -6,26 +6,12 @@
  Description: Find where and how medias are used across your site.
  Author: Be API Technical team
  Author URI: https://beapi.fr
+ Contributors: Maxime Culea
+ 
  Domain Path: languages
  Text Domain: bea-find-media
-
  ----
-
  Copyright 2017 Be API Technical team (human@beapi.fr)
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 // don't load directly
