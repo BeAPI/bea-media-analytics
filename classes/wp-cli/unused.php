3a7d42b596caf38881bc69254e7ade68bd76ea68
Maxime Culea <culea.maxime@gmail.com>
2018-02-26 20:29:09 +0100

    #25 : finish wp cli listing of unused medias

--- classes/wp-cli/unused.php
+++ classes/wp-cli/unused.php
@@ -1,5 +1,7 @@
 <?php namespace BEA\Media_Analytics\WP_Cli;
 
+use BEA\Media_Analytics\Helper\API;
+
 class Unused extends \WP_CLI_Command {
 
 	/**
@@ -17,10 +19,54 @@ class Unused extends \WP_CLI_Command {
 	 * @synopsis
 	 */
 	function unused( $args ) {
-		if ( empty( $args ) || $args['action'] ) {
+		list( $action ) = $args;
+		if ( empty( $action ) ) {
 			\WP_CLI::error( "No action provided ! Choose between 'list' or 'deleted'. \n Usage : wp bea_media_analytics unused <action>" );
 		}
 
-		\WP_CLI::success( 'Done' );
+		switch ( $action ) {
+			case 'list' :
+				$this->list();
+				break;
+			case 'delete' :
+				$this->delete();
+				break;
+		}
 	}
+
+	/**
+	 * Handle wp cli to list unused medias
+	 *
+	 * @since future
+	 *
+	 * @author Maxime CULEA
+	 */
+	private function list() {
+		$table  = [];
+		$medias = API::get_unused_media();
+		if ( ! empty( $medias ) ) {
+			foreach ( $medias as $media_id ) {
+				$table[] = [
+					'blog_id'     => get_current_blog_id(),
+					'media_id'    => $media_id,
+					'media_title' => get_the_title( $media_id ),
+				];
+			}
+		}
+
+		if ( ! empty( $table ) ) {
+			\WP_CLI\Utils\format_items( 'table', $table, [ 'blog_id', 'media_id', 'media_title' ] );
+		} else {
+			\WP_CLI::error( "wp bea_media_analytics unused list : All media are used." );
+		}
+	}
+
+	/**
+	 * Handle wp cli to list unused medias
+	 *
+	 * @since future
+	 *
+	 * @author Maxime CULEA
+	 */
+	private function delete() {}
 }
