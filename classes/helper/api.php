91ad962c76c1ffec75c00ea4efb709a96784d92e
Maxime Culea <culea.maxime@gmail.com>
2018-02-26 20:28:48 +0100

    #25 : add api for unused media

---
+++ classes/helper/api.php
@@ -0,0 +1,27 @@
+<?php namespace BEA\Media_Analytics\Helper;
+
+use BEA\Media_Analytics\DB;
+
+class API {
+	public static function get_unused_media() {
+		$_medias = [];
+		$medias = new \WP_Query( [
+			'post_type'     => 'attachment',
+			'post_status'   => 'any',
+			'no_found_rows' => true,
+			'fields'        => 'ids',
+			'nopaging'      => true
+		] );
+		if ( $medias->have_posts() ) {
+			foreach ( $medias->posts as $media_id ) {
+				if ( ! empty( DB::get_data( $media_id ) ) ) {
+					continue;
+				}
+
+				$_medias[] = $media_id;
+			}
+		}
+
+		return $_medias;
+	}
+}
