d1b14f7d12793a0a839970e9754239b53c67b6f1
Maxime Culea <culea.maxime@gmail.com>
2018-03-28 01:23:33 +0200

    #59 : refactor way wp cli commands are registered to not overwrite existing commands

--- classes/wp-cli/main.php
+++ classes/wp-cli/main.php
@@ -10,7 +10,8 @@ class Main {
 		if ( ! defined( 'WP_CLI' ) ) {
 			return;
 		}
-		\WP_CLI::add_command( 'bea_media_analytics index_site', 'BEA\Media_Analytics\WP_Cli\Index_Site' );
-		\WP_CLI::add_command( 'bea_media_analytics', 'BEA\Media_Analytics\WP_Cli\Unused' );
+		\WP_CLI::add_command( 'bea_media_analytics index_site', [ 'BEA\Media_Analytics\WP_Cli\Index_Site', 'index_site' ] );
+		\WP_CLI::add_command( 'bea_media_analytics unused enumerate', [ 'BEA\Media_Analytics\WP_Cli\Unused', 'enumerate' ] );
+		\WP_CLI::add_command( 'bea_media_analytics unused delete', [ 'BEA\Media_Analytics\WP_Cli\Unused', 'delete'] );
 	}
 }
