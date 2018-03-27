3f039ab2639f1c16c09606e93d06ca4a06186a82
Amaury BALMER <amaury@beapi.fr>
2018-03-27 15:37:47 +0200

    add query api for get unsed medias

---
+++ classes/api/query.php
@@ -0,0 +1,88 @@
+<?php namespace BEA\Media_Analytics\API;
+
+use BEA\Media_Analytics\DB_Table;
+use BEA\Media_Analytics\Singleton;
+
+class Query {
+	use Singleton;
+
+	protected function init() {
+		add_action( 'query_vars', array( __CLASS__, 'query_vars' ), 10 );
+		add_filter( 'posts_join', array( __CLASS__, 'posts_join' ), 10, 2 );
+		add_filter( 'posts_where', array( __CLASS__, 'posts_where' ), 10, 2 );
+		add_filter( 'posts_groupby', array( __CLASS__, 'posts_groupby' ), 10, 2 );
+	}
+
+	/**
+	 * Append a custom query var for this new filter on WP_Query
+	 *
+	 * @param array $vars
+	 *
+	 * @return array
+	 */
+	public static function query_vars( $vars ) {
+		$vars[] = "bea_media_analytics";
+
+		return $vars;
+	}
+
+	/**
+	 * Add join with plugin table for filter unused or used medias
+	 *
+	 * @param $join
+	 * @param WP_Query $query
+	 *
+	 * @return string
+	 */
+	public static function posts_join( $join, \WP_Query $query ) {
+		global $wpdb;
+
+		if ( empty( $query->get( 'bea_media_analytics' ) ) ) {
+			return $join;
+		}
+
+		$join_type = $query->get( 'bea_media_analytics' ) == 'unused' ? 'LEFT' : 'INNER';
+
+		$join .= " $join_type JOIN " . DB_Table::get_instance()->get_table_name() . " AS bma ON ( $wpdb->posts.ID = bma.media_id AND bma.blog_id = " . get_current_blog_id() . " ) ";
+
+		return $join;
+	}
+
+	/**
+	 * Add join with plugin table for unused medias
+	 *
+	 * @param $where
+	 * @param WP_Query $query
+	 *
+	 * @return string
+	 */
+	public static function posts_where( $where, \WP_Query $query ) {
+		if ( empty( $query->get( 'bea_media_analytics' ) ) || $query->get( 'bea_media_analytics' ) != 'unused' ) {
+			return $where;
+		}
+
+		$where .= " AND bma.media_id IS NULL ";
+
+		return $where;
+	}
+
+	/**
+	 * Add join with plugin table for unused medias
+	 *
+	 * @param $groupby
+	 * @param WP_Query $query
+	 *
+	 * @return string
+	 */
+	public static function posts_groupby( $groupby, \WP_Query $query ) {
+		global $wpdb;
+
+		if ( empty( $query->get( 'bea_media_analytics' ) ) || $query->get( 'bea_media_analytics' ) != 'used' ) {
+			return $groupby;
+		}
+
+		$groupby = " $wpdb->posts.ID ";
+
+		return $groupby;
+	}
+}
