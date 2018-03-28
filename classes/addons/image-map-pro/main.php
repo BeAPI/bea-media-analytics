7a064e42786f2406a8a9601bff0e4d736e0556d8
Maxime Culea <culea.maxime@gmail.com>
2018-03-28 11:01:41 +0200

    #51 : moove get_attachment_id_from_url method into helper class

--- classes/addons/image-map-pro/main.php
+++ classes/addons/image-map-pro/main.php
@@ -23,6 +23,10 @@ class Main {
 	 * Add image map pro indexation into WP-CLi command
 	 *
 	 * @param integer $i
+	 *
+	 * @since future
+	 *
+	 * @author Amaury Balmer
 	 */
 	public function index_site( $i ) {
 		\WP_CLI::warning( 'Image Map Pro indexing.' );
@@ -39,6 +43,10 @@ class Main {
 	 * @param $types
 	 * @param $content_id
 	 * @param $media_id
+	 *
+	 * @since future
+	 *
+	 * @author Amaury Balmer
 	 *
 	 * @return string
 	 */
@@ -57,6 +65,10 @@ class Main {
 	 *
 	 * @param $value
 	 * @param $old_value
+	 *
+	 * @since future
+	 *
+	 * @author Amaury Balmer
 	 *
 	 * @return mixed
 	 */
@@ -88,50 +100,15 @@ class Main {
 		}
 
 		// Loop on medias for get ID instead URL
-		$medias = array_map( array( $this, 'get_attachment_id_from_url' ), $medias );
+		$medias = array_map( [ 'BEA\Media_Analytics\Helper\Helper', 'get_attachment_id_from_url' ], $medias );
 		$medias = array_filter( $medias );
 
 		// Validate image IDs
-		$medias = Helper::merge_old_with_new( array(), $medias, 'image-map-pro' );
+		$medias = Helper::merge_old_with_new( [], $medias, 'image-map-pro' );
 		$medias = Helpers::check_image_ids( $medias );
 
 		DB::insert( $medias, 'image-map-pro', 'image-map-pro' );
 
 		return $value;
 	}
-
-	/**
-	 * Transform full URL in attachment ID
-	 * credit : https://philipnewcomer.net/2012/11/get-the-attachment-id-from-an-image-url-in-wordpress/
-	 *
-	 * @param string $attachment_url
-	 *
-	 * @return int
-	 */
-	public function get_attachment_id_from_url( $attachment_url = '' ) {
-		global $wpdb;
-
-		// If there is no url, return.
-		if ( '' == $attachment_url ) {
-			return 0;
-		}
-
-		// Get the upload directory paths
-		$upload_dir_paths = wp_upload_dir();
-
-		// Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image
-		if ( false !== strpos( $attachment_url, $upload_dir_paths['baseurl'] ) ) {
-			// If this is the URL of an auto-generated thumbnail, get the URL of the original image
-			$attachment_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
-
-			// Remove the upload path base directory from the attachment URL
-			$attachment_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $attachment_url );
-
-			// Finally, run a custom database query to get the attachment ID from the modified attachment URL
-			return (int) $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = '%s' AND wposts.post_type = 'attachment'", $attachment_url ) );
-		}
-
-		return 0;
-	}
-
 }
