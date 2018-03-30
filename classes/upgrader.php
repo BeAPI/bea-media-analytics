ed6eeb5ba04f3d509a702129691e26bbf81a5465
Maxime Culea <culea.maxime@gmail.com>
2018-03-30 23:41:58 +0200

    #50 : Finally add an upgrader class

---
+++ classes/upgrader.php
@@ -0,0 +1,31 @@
+<?php namespace BEA\Media_Analytics;
+
+class Upgrader {
+	use Singleton;
+
+	protected function init() {
+		add_action( 'upgrader_process_complete', [ $this, 'plugin_updated_actions' ], 10, 2 );
+	}
+
+	/**
+	 * On plugin update, launch custom actions as reindexing contents
+	 *
+	 * @param $upgrader
+	 * @param $options
+	 *
+	 * @since  future
+	 * @author Maxime CULEA
+	 */
+	private function plugin_updated_actions( $upgrader, $options ) {
+		if ( 'plugin' !== $options['type'] || 'update' !== $options['action'] || ! in_array( BEA_MEDIA_ANALYTICS_PLUGIN_DIRNAME, $options['plugins'] ) ) {
+			return;
+		}
+
+		// Update option for forcing cron schedule
+		update_option( 'bea_media_analytics_index', false );
+		Crons::schedule();
+
+		// Set this transient to allow to show admin notice that indexing will manage soon
+		set_transient( 'bma_notice_plugin_updated', true, HOUR_IN_SECONDS );
+	}
+}
