98eb51b8aa6b8cbb4b419c8bb8531ea4697df779
Maxime Culea <culea.maxime@gmail.com>
2018-04-15 21:05:12 +0200

    #39 : don't format anymore data as they are threated into wp list table prepare items

--- classes/main.php
+++ classes/main.php
@@ -6,51 +6,10 @@ class Main {
 	use Singleton;
 
 	protected function init() {
-		add_filter( 'bea.media_analytics.db.get_data', [ __CLASS__, 'format_indexed_values' ], 100 );
 		add_action( 'init', [ $this, 'init_translations' ] );
 
 		// JS i18n
 		add_action( 'admin_enqueue_scripts', [ $this, 'localize_scripts' ], 40 );
-	}
-
-	/**
-	 * Format the data :
-	 *
-	 * {object_type} : [
-	 *      {media_id} : [
-	 *          {object_id} : [ {type}, {type} ]
-	 *      ]
-	 * ]
-	 *
-	 * @param array $data
-	 *
-	 * @since  1.0.O
-	 * @author Maxime CULEA
-	 *
-	 * @return array
-	 */
-	public static function format_indexed_values( $data ) {
-		if ( empty( $data ) ) {
-			return [];
-		}
-
-		$out = [];
-		foreach ( $data as $_d ) {
-			if ( isset( $out[ $_d->object_type ][ $_d->media_id ][ $_d->object_id ] ) && in_array( $_d->type, $out[ $_d->object_type ][ $_d->media_id ][ $_d->object_id ] ) ) {
-				// Already exists
-				continue;
-			}
-
-			if ( empty( $out[ $_d->object_type ][ $_d->media_id ][ $_d->object_id ] ) ) {
-				// First value
-				$out[ $_d->object_type ][ $_d->media_id ][ $_d->object_id ][] = $_d->type;
-			} else {
-				// Adding to the others
-				array_push( $out[ $_d->object_type ][ $_d->media_id ][ $_d->object_id ], $_d->type );
-			}
-		}
-
-		return $out;
 	}
 
 	/**
@@ -78,7 +37,7 @@ class Main {
 
 		$out = [];
 		foreach ( $data as $blog_id => $_d ) {
-			$out[ $blog_id ] = self::format_indexed_values( $_d );
+			$out[ $blog_id ] = $_d;
 		}
 
 		return $out;
