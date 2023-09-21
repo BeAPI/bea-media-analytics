020633c7d1e4df85003850d578c9b0f41f346e9d
f2cmbeapi <55753497+f2cmbeapi@users.noreply.github.com>
2023-09-21 17:35:59 +0200

    compat/fix-singleton-funcions-declaration

--- classes/singleton.php
+++ classes/singleton.php
@@ -46,16 +46,18 @@ trait Singleton {
 	/**
 	 * prevent the instance from being cloned
 	 *
-	 * @return void
+	 * @throws Exception
 	 */
-	final public function __clone() {
-	}
+    private function __clone() {
+        throw new \Exception( 'Cannot clone singleton' );
+    }
 
 	/**
 	 * prevent from being unserialized
 	 *
-	 * @return void
+	 * @throws Exception
 	 */
-	final public function __wakeup() {
-	}
+    final public function __wakeup() {
+        throw new \Exception( 'Cannot serialize singleton' );
+    }
 }
